16a520db8eabc50415b2626d1e64a5af3fbcdd73
seabat <ryouta.nakasnishi.86@gmail.com>
2023-09-23 16:05:54 +0900

    LibrariesForLibs' can't be called in this context by implicit receiver を解決

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,9 +7,18 @@ buildscript {
 }
 
 plugins {
+    @Suppress("DSL_SCOPE_VIOLATION")
     alias(libs.plugins.android.application) apply false
+
+    @Suppress("DSL_SCOPE_VIOLATION")
     alias(libs.plugins.android.library) apply false
+
+    @Suppress("DSL_SCOPE_VIOLATION")
     alias(libs.plugins.kotlin) apply false
+
+    @Suppress("DSL_SCOPE_VIOLATION")
     alias(libs.plugins.hilt) apply false
+
+    @Suppress("DSL_SCOPE_VIOLATION")
     alias(libs.plugins.secrets.gradle) apply false
 }
